be41f1c5b1ba0b628288c3e23e528b6cba0b1855
Pedro Araujo da Silva <pedroaraujodasilva@MacBook-Air-de-KRISHNA.local>
2026-05-17 17:13:20 -0300

    deploy: static build 2026-05-17_17:13

---
+++ _next/static/I2fegcKCqBDmZ7onlIIla/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fpmo-dashboard\u002Fprocessos\u002F[id]","\u002Fpmo-dashboard\u002Fprocessos\u002F[id]\u002Fedit"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
